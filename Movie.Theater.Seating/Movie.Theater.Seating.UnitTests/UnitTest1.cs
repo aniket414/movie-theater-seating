724dbbc0c420923f26977dfce0d967eb952ee4c6
Aniket Pandey <aniket414@gmail.com>
2022-03-14 02:18:39 -0400

    added unit test base project

---
+++ Movie.Theater.Seating/Movie.Theater.Seating.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Movie.Theater.Seating.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
