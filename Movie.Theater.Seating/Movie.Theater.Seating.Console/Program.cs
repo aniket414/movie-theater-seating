d6e1fa1d4183b95f9756e4e12fe2de7a3d241335
Aniket Pandey <aniket414@gmail.com>
2022-03-14 04:18:06 -0400

    added all the base setup and tested via terminal

--- Movie.Theater.Seating/Movie.Theater.Seating.Console/Program.cs
+++ Movie.Theater.Seating/Movie.Theater.Seating.Console/Program.cs
@@ -1,13 +1,29 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Movie.Theater.Seating.Service;
 
 namespace Movie.Theater.Seating
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serviceProvider = SetupDI();
+
+            var bookingService = serviceProvider.GetService<IBookingService>();
+            var output = bookingService.FillRows("R001", 2);
+
+            Console.WriteLine("Done " + output);
             Console.WriteLine(args[0]);
+        }
+
+        private static ServiceProvider SetupDI()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IBookingService, BookingService>()
+                .BuildServiceProvider();
+
+            return serviceProvider;
         }
     }
 }
---
+++ Movie.Theater.Seating/Movie.Theater.Seating.UnitTests/BookingServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+using Movie.Theater.Seating.Service;
+
+namespace Movie.Theater.Seating.UnitTests
+{
+    public class BookingServiceTests
+    {
+        BookingService bookingService = new BookingService();
+
+        [Fact]
+        public void SampleTest()
+        {
+            Assert.Equal(12, bookingService.FillRows("R001", 12));
+        }
+    }
+}
